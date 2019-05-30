68b100a68fe8df959d8ac5409313935685df56de
Ryan Pethel <arpethel@gmail.com>
2019-05-30 03:59:32 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.integer "price"
     t.string "size"
-    t.string "image_url"
+    t.string "imageURL"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
